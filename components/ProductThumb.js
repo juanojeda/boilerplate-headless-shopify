dbde55b66a3bfdc2f37f1eeeb21b0a1c893fbc9a
Juan Ojeda <juan@juanojeda.com>
2021-01-14 23:14:19 +1100

    simplify price on product thumb

--- components/ProductThumb.js
+++ components/ProductThumb.js
@@ -2,7 +2,6 @@ import React, { useContext } from "react";
 import { ShopifyContext } from "../hooks/withShopifyContext";
 import { formatPrice } from "../utils/formatPrice";
 
-const extractPrice = (variant) => variant[0].priceV2;
 const ProductThumb = ({
   id,
   title,
@@ -13,6 +12,7 @@ const ProductThumb = ({
 }) => {
   const { addItem } = useContext(ShopifyContext);
   const onAddToCart = () => addItem(variants[0].id);
+  const variant = variants[0];
 
   return (
     <div>
@@ -21,7 +21,7 @@ const ProductThumb = ({
       <img src={images[0].originalSrc} alt={images[0].altText} height="200" />
       {!availableForSale && <div>sold out</div>}
       <div>stock: {totalInventory}</div>
-      <div>Price: {formatPrice(extractPrice(variants))}</div>
+      <div>Price: {formatPrice(variant.priceV2)}</div>
       <button onClick={onAddToCart}>Add to cart</button>
     </div>
   );
