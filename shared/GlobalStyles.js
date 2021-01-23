7ac7642b9da8c4a60b1f95d4a6bb488aabfcd2e8
Juan Ojeda <juan@juanojeda.com>
2021-01-23 22:40:05 +1100

    add font size reset

--- shared/GlobalStyles.js
+++ shared/GlobalStyles.js
@@ -1,9 +1,14 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
+  html {
+    font-size: 10px;
+  }
+
   body {
     font-family: "Public Sans";
     font-weight: 400;
+    font-size: 1.6rem;
   }
 `;
 
