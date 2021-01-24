48465648ff1fd2fdc209a2efdc4891ff315e496a
Juan Ojeda <juan@juanojeda.com>
2021-01-24 15:08:09 +1100

    add stub pages for about and knifecare

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+        repudiandae delectus aliquid facere at cupiditate impedit quia ullam?
+        Enim eos dolores laudantium molestiae ut mollitia, quaerat numquam
+        corporis laborum et.
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ pages/knife-care.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const KnifeCarePage = () => {
+  return (
+    <div>
+      <h1>Knife Care</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores harum,
+        neque sint, ad dolorum labore quas at aliquid quidem reprehenderit quam
+        perferendis quia corporis minima eos sapiente culpa repudiandae
+        voluptate?
+      </p>
+    </div>
+  );
+};
+
+export default KnifeCarePage;
